95701dd16bc2568752f01d144e8c4da8f402db0b
Yami Thalyssa <thalyssa2.0@hotmail.com>
2018-09-10 08:53:45 -0300

    Create positive_queue.c

---
+++ positive_queue.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct node node;
+typedef struct queue queue;
+
+struct node{
+	int data;
+	node *next;
+};
+
+struct queue{
+	node *first;
+	node *last;
+	int size;
+};
+
+queue* create_queue();
+bool isEmpty(queue *q);
+node* create_node(int data);
+void enqueue(queue *q, int data);
+int dequeue(queue *q);
+void print_queue(queue *q);
+queue *positive(queue *q);
+
+
+int main(){
+
+	int n;
+	queue *initQ = create_queue();
+	queue *positiveQueue = create_queue();
+
+	enqueue(initQ, 1);
+	enqueue(initQ, 2);
+	enqueue(initQ, -2);
+	enqueue(initQ, 0);
+
+	positiveQueue = positive(initQ);
+
+	print_queue(positiveQueue);
+
+
+	return 0;
+}
+
+queue *create_queue(){
+	queue *q = (queue*)malloc(sizeof(queue));
+	q->first = NULL;
+	q->last = NULL;
+	q->size = 0;
+
+	return q;
+}
+
+bool isEmpty(queue *q){
+	if(q->first == NULL){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+node* create_node(int data){
+	node *new_node = (node*)malloc(sizeof(node));
+	new_node->data = data;
+	new_node->next = NULL;
+
+	return new_node;
+
+}
+
+void enqueue(queue *q, int data){
+	node *new_node = create_node(data);
+	if(isEmpty(q)){
+		q->last = new_node;
+		q->first = q->last;
+	}else{
+		q->last->next = new_node;
+		q->last = new_node;
+	}
+	q->size++;
+}
+
+int dequeue(queue *q){
+	if(isEmpty(q)){
+		printf("Fila vazia!\n");
+		return -1;
+	}else{
+		int item = q->first->data;
+		node *aux = q->first;
+
+		q->first = q->first->next;
+		q->size--;
+
+		if(q->size == 0){
+			q->last = NULL;
+		}
+
+		free(aux);
+		return item;
+	}
+}
+
+void print_queue(queue *q){
+	if(isEmpty(q)){
+		printf("Fila Vazia!!\n");
+	}else{
+		queue *aux = (queue*)malloc(sizeof(queue));
+		aux->first = q->first;
+		while(aux->first!=NULL){
+			printf("%d \n", aux->first->data);
+			aux->first = aux->first->next;
+		}
+	}
+}
+
+queue* positive(queue *q){
+	queue *aux_queue = create_queue();
+
+	while(q->size>0){
+		int data = dequeue(q);
+		if(data>0){
+			enqueue(aux_queue, data);
+		}
+	}
+
+	return aux_queue;
+
+}
